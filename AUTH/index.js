f941a3f15eeb8b4981836f69231b90a4090bc461
Aakriti thapa <thapaaakriti570@gmail.com>
2022-10-14 16:27:11 +0545

    first

---
+++ AUTH/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
+
+  () => console.log("connected to db")
+);
+//middleware
+app.use(express.json());
+
+app.use("/api/user", authRoute);
+app.listen(8000, () => console.log("server Up and running"));
